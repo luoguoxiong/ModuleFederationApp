6b9da68b9a2b7be2e562595e8e495e4e12b857e4
罗国雄 <1025558554@qq.com>
2021-12-10 18:31:44 +0800

    feat: any

--- app2/src/global.d.ts
+++ app2/src/global.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="react" />
 
-declare module "libary/react" {
+declare module 'libary/react' {
     export = React;
 }
 
-declare module "libary/react-dom"{
+declare module 'libary/react-dom'{
     export = ReactDOM
 }
